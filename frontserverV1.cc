5b8261485d9b85dc2aff920553f961a44f2fa6f4
Abhishek Goyal <goyal.abhi1905@gmail.com>
2024-03-30 15:47:25 -0400

    Signup-Login

--- frontserverV1.cc
+++ frontserverV1.cc
@@ -209,7 +209,7 @@ void *worker(void *arg) {
     }
 
     //If the data is received successfully, add to tempcommand and process the command
-    tmpcommand += buffer;
+    tmpcommand = buffer;
     string content = "";
     cout<<tmpcommand<<endl;
     // check the type of the method, it is till the first space. Could be GET, POST, PUT, DELETE, HEAD
@@ -238,7 +238,7 @@ void *worker(void *arg) {
     
     // string content = "Hello from the C++ server using pthread";
     // string httpResponse = "HTTP/1.1 200 OK\r\nDate: Fri, 31 Dec 1999 23:59:59 GMT\r\nContent-Type: text/plain\r\nContent-Length: " + to_string(content.length()) + "\r\n\r\n" + content;//Hello from the C++ server using pthread!";
-    cout<<content<<endl;
+    //cout<<content<<endl;
     send(fd, content.c_str(), content.length(),0);
     tmpcommand = "";
   
--- getMethod.cc
+++ getMethod.cc
@@ -13,18 +13,33 @@ string getMethodHandler(string command) {
     command = command.substr(0, command.find(" "));
 
     // handle the commandType and return the appropriate response
-    if (command == "/") {
+    if (command == "/" || command == "/signup" || command == "/login") {
         ifstream ifs("./pages/index.html");
         string homepage((istreambuf_iterator<char>(ifs)), (istreambuf_iterator<char>()));
         string httpResponseHeader = "HTTP/1.1 200 OK\r\n"
                                    "Content-Type: text/html\r\n"
                                    "Content-Length: " + std::to_string(homepage.size()) + "\r\n\r\n";
         return httpResponseHeader + homepage;
-    } else if (command == "/about") {
-        return "HTTP/1.1 200 OK\nContent-Type: text/html\n\n<h1>About Us</h1>";
-    } else if (command == "/login") {
+    } 
+    // else if (command == "/login") {
+    //     ifstream ifs("./pages/index.html");
+    //     string homepage((istreambuf_iterator<char>(ifs)), (istreambuf_iterator<char>()));
+    //     string httpResponseHeader = "HTTP/1.1 200 OK\r\n"
+    //                                "Content-Type: text/html\r\n"
+    //                                "Content-Length: " + std::to_string(homepage.size()) + "\r\n\r\n";
+    //     return httpResponseHeader + homepage;
+    // } 
+    else if (command == "/about") {
         return "HTTP/1.1 200 OK\nContent-Type: text/html\n\n<h1>Contact Us</h1>";
-    } else {
+    } else if (command == "/inbox") {
+        return "HTTP/1.1 200 OK\nContent-Type: text/html\n\n<h1>Inbox Us</h1>";
+    } else if (command == "/downloadFile") {
+        return "HTTP/1.1 200 OK\nContent-Type: text/html\n\n<h1>downloadFile</h1>";
+    } else if (command == "/listFile") {
+        return "HTTP/1.1 200 OK\nContent-Type: text/html\n\n<h1>listFile</h1>";
+    }
+
+    else {
         return "HTTP/1.1 404 Not Found\nContent-Type: text/html\n\n<h1>404 Not Found</h1>";
     }
 
